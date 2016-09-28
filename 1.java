b41f615509a293b81a74843e77ea93487f82f00f
Andrew Reid <Andrewvreid02@gmail.com>
2016-09-28 19:25:42 +0100

    First work of the new school year
    (school made us put hello world like 200 tines)

---
+++ 1.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("hello world");
+    System.out.print();
+    System.out.println("Hello, World!");
+    System.out.print("Hello,");
+    System.out.print("");
+    System.out.print("World!");
+    System.out.println("Hello, world!");
+  }
+}
